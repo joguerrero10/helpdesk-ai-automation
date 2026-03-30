eca6e5254525d01914bf56a1e427a530099c788d
joguerrero10 <joelg1014@hotmail.com>
2026-03-30 18:50:53 -0500

    se elimino comentario

--- src/integrations/ai/ai.service.ts
+++ src/integrations/ai/ai.service.ts
@@ -1,33 +1,3 @@
-// import Groq from "groq-sdk";
-
-// const client = new Groq({
-//   apiKey: process.env.GROQ_API_KEY,
-// });
-
-// class AIService {
-//   async generateResponse(message: string): Promise<string> {
-//     const completion = await client.chat.completions.create({
-//       model: "llama-3.1-8b-instant",
-//       messages: [
-//         {
-//           role: "system",
-//           content:
-//             "Eres un experto agente de soporte técnico especializado en resolver problemas relacionados con software y hardware. Proporciona respuestas claras, concisas y útiles para ayudar a los usuarios a solucionar sus problemas técnicos de manera eficiente.",
-//         },
-//         {
-//           role: "user",
-//           content: message,
-//         },
-//       ],
-//       max_tokens: 200,
-//       temperature: 0.7,
-//     });
-
-//     return completion.choices[0].message.content || "No entendí tu mensaje";
-//   }
-// }
-
-// export default new AIService();
 import Groq from "groq-sdk";
 import { ChatCompletionMessageParam } from "groq-sdk/resources/chat/completions";
 import sessionService from "../../services/session.service";
